9c493d19ffe302dc2485c8631f18e6e838760e45
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-11-25 13:49:51 -0500

    Update basicpeople.sh

--- basicpeople.sh
+++ basicpeople.sh
@@ -3,7 +3,7 @@ echo "Installing Spotify, Discord, and Steam..."
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
-curl "https://discord.com/api/download?platform=linux&format=deb" > /tmp/discord.deb
+curl "https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.deb" > /tmp/discord.deb
 sudo dpkg -i /tmp/discord.deb
 sudo apt --fix-broken install
-sudo apt install steam-installer 
+sudo apt install steam-installer discord 
