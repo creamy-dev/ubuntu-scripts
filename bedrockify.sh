96487dd0fddee5a94528f82e71cb1ff621cc53dd
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-11-25 13:17:30 -0500

    create script to install bedrock linux

---
+++ bedrockify.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install curl
+curl "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.24/bedrock-linux-0.7.24-x86_64.sh" > /tmp/bedrock.sh
+chmod +x /tmp/bedrock.sh
+sudo bash /tmp/bedrock.sh
