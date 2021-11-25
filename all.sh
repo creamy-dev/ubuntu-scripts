f345926f3521dd66b724a03c647873afa4ad6638
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-11-25 13:47:51 -0500

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+echo "Be sure to save all your files!"
+sleep 3s
 sudo apt install curl -y
 curl "https://raw.githubusercontent.com/creamy-dev/ubuntu-scripts/main/debloat.sh" > /tmp/debloat.sh
 chmod +x /tmp/debloat.sh
 curl "https://raw.githubusercontent.com/creamy-dev/ubuntu-scripts/main/basicpeople.sh" > /tmp/basicpeople.sh
 chmod +x /tmp/basicpeople.sh
-curl "https://raw.githubusercontent.com/creamy-dev/ubuntu-scripts/main/steam.sh" > /tmp/steam.sh
-chmod +x /tmp/steam.sh
 curl "https://raw.githubusercontent.com/creamy-dev/ubuntu-scripts/main/bedrockify.sh" > /tmp/bedrockify.sh
 chmod +x /tmp/bedrockify.sh
 bash /tmp/debloat.sh
 bash /tmp/basicpeople.sh
-bash /tmp/steam.sh
 bash /tmp/bedrockify.sh
 sudo reboot
