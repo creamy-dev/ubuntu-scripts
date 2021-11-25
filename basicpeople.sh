f8435f2c27d9ac0db37c5224b6a2beaf2d2642e0
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-11-25 13:22:30 -0500

    create installer for Spotify and Discord

---
+++ basicpeople.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Installing Spotify, and Discord..."
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+curl "https://discord.com/api/download?platform=linux&format=deb" > /tmp/discord.deb
+sudo dpkg -i /tmp/discord.deb
+sudo apt --fix-broken install
