3f88a9ab319bd92d168bc4328802d7508ca9b383
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-11-25 13:35:37 -0500

    fix bedrockify

--- bedrockify.sh
+++ bedrockify.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 sudo apt install curl
 curl "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.24/bedrock-linux-0.7.24-x86_64.sh" > /tmp/bedrock.sh
+curl -s https://api.github.com/repos/bedrocklinux/bedrocklinux-userland/releases/latest \
+| grep "browser_download_url.*-x86_64.sh" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+mv bedrock* /tmp/bedrock.sh
 chmod +x /tmp/bedrock.sh
 yes "Not reversible!" | sudo bash /tmp/bedrock.sh
