b3ccddb0aaaabd5a6a941a1aef51142d0bf97806
Greyson <56458201+creamy-dev@users.noreply.github.com>
2021-11-25 13:24:45 -0500

    Update bedrockify.sh

--- bedrockify.sh
+++ bedrockify.sh
@@ -2,4 +2,4 @@
 sudo apt install curl
 curl "https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.24/bedrock-linux-0.7.24-x86_64.sh" > /tmp/bedrock.sh
 chmod +x /tmp/bedrock.sh
-sudo bash /tmp/bedrock.sh
+yes "Not reversible!" | sudo bash /tmp/bedrock.sh
